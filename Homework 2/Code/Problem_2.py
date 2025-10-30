30c8bf175b8a56ee11fdfb7e5ed14aefbc28ecc7
东风平夷 <98257102+Schuyn@users.noreply.github.com>
2025-10-30 14:37:24 -0400

    work on problem2

---
+++ Homework 2/Code/Problem_2.py
@@ -0,0 +1,8 @@
+'''
+Author: Chuyang Su cs4570@columbia.edu
+Date: 2025-10-30 14:36:11
+LastEditTime: 2025-10-30 14:36:24
+FilePath: /Unsupervised-Learning-Homework/Homework 2/Code/Problem_2.py
+Description: 
+
+'''
